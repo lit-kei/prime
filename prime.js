60248cfd689b1f3f6bd794b5ba25c3d6841eab1a
lit-kei <puyoteto123@yahoo.co.jp>
2025-07-15 21:33:15 +0900

    update

--- prime.js
+++ prime.js
@@ -1,14 +1,20 @@
-const NoP = 6; // Number of Primes
+let NoP = 5; // Number of Primes
 const primes = [
     { n: 2, p: 3 },
     { n: 3, p: 3 },
-    { n: 5, p: 1 },
+    { n: 5, p: 2 },
     { n: 7, p: 1 },
-    { n: 11, p: 1 }
+    { n: 11, p: 1 },
+    { n: 13, p: 1 },
+    { n: 17, p: 1 },
+    { n: 19, p: 1 },
+    { n: 23, p: 1 },
+    { n: 29, p: 1 }
 ];
-
+let kind = 6;
 let isProcessing = false;
-let sum = primes.reduce((acc, prime) => acc + prime.p, 0);
+let isAble = true;
+let finish = true;
 let interval = 4000;
 let pArr = [];
 let txt = "";
@@ -18,6 +24,12 @@ const answer = document.getElementById('answer');
 const button = document.getElementById('button');
 const progress = document.getElementById('progress');
 
+let sum = 0;
+for (let i = 0; i < primes.length; i++) {
+    sum += primes[i].p;
+    primes[i].s = sum;
+}
+
 async function processLoop() {
     while (isProcessing) {
         let num = 1;
@@ -26,7 +38,7 @@ async function processLoop() {
         answer.innerHTML = "&#x00A0";
 
         for (let i = 0; i < NoP; i++) {
-            let rand = Math.floor(Math.random() * sum);
+            let rand = Math.floor(Math.random() * primes[kind].s);
             let j = 0;
             for (j = 0; j < primes.length; j++) {
                 rand -= primes[j].p;
@@ -38,7 +50,7 @@ async function processLoop() {
         pArr.sort((a, b) => a - b);
 
         question.textContent = num;
-        document.getElementById('progress').style.transition = `width ${interval / 1000}s linear`;
+        progress.style.transition = `width ${interval / 1000}s linear`;
         progress.style.width = '100%';
         await new Promise(resolve => setTimeout(resolve, interval));
         for (let i = 0; i < pArr.length; i++) {
@@ -48,32 +60,59 @@ async function processLoop() {
             }
         }
         answer.textContent = txt;
-        document.getElementById('progress').style.transition = `width 5s linear`;
+        progress.style.transition = `width 5s linear`;
         progress.style.width = '0%';
         progress.classList.add('back');
 
         await new Promise(resolve => setTimeout(resolve, 5000));
         progress.classList.remove('back');
-
+        if (!isAble) break;
     }
+    question.innerHTML = "&#x00A0";
+    answer.innerHTML = "&#x00A0";
+    progress.style.transition = 'none';
+    progress.style.width = '0%';
+    progress.classList.remove('back');
+    button.textContent = 'スタート';
+    finish = true;
 }
 
 button.addEventListener('click', () => {
     isProcessing = !isProcessing;
     button.classList.toggle('process');
-    button.textContent = isProcessing ? 'ストップ' : 'スタート';
+    button.textContent = isProcessing ? '終わる' : 'スタート';
 
     if (isProcessing) {
-        processLoop(); // 非同期処理開始
+        isAble = true;
+        if (finish) {
+            finish = false;
+            processLoop(); // 非同期処理開始
+        }
+    } else {
+        isAble = false;
+        if (finish == false) {
+            button.textContent = '再開';
+        }
     }
+
 });
 
 
-document.getElementById('second').addEventListener('input', () => {
-    const data = document.getElementById('second').value;
+document.getElementById('speed').addEventListener('input', () => {
+    const data = document.getElementById('speed').value;
     if (data >= 1 && data <= 10) {
         interval = data * 1000;
         document.getElementById('progress').style.transition = `width ${data}s linear`;
-        document.getElementById('interval').textContent = String(data);
+        document.getElementById('speed-span').textContent = "：" + String(data);
     }
+});
+document.getElementById('num').addEventListener('input', () => {
+    const data = document.getElementById('num').value;
+    NoP = data;
+    document.getElementById('num-span').textContent = "：" + String(data);
+});
+document.getElementById('kind').addEventListener('input', () => {
+    const data = document.getElementById('kind').value;
+    kind = data - 1;
+    document.getElementById('kind-span').textContent = "：" + String(data);
 });
